f983dbd9886e6901223f417e2552953c6307fcd0
talithasyafiyah <talithasyafiyah1112@gmail.com>
2022-05-05 22:38:23 +0700

    lagiii

--- homepage/new_drugs.php
+++ homepage/new_drugs.php
@@ -4,8 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="bootstrap-5.1.3/dist/css/bootstrap.css">
-    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>Drugs Approval</title>
     <link rel="icon" href="img/drugs1.png" type="image/png">
     <script src="https://kit.fontawesome.com/769f430edb.js" crossorigin="anonymous"></script>
@@ -93,9 +92,6 @@ include 'layout/header.php';
         </div>
         </div>
     </div>
-<?php
-include 'layout/footer.php';
-?>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
 </body>
