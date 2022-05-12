a2b3824ef9ae875608f806034bbf7c6d01756bc8
talithasyafiyah <talithasyafiyah1112@gmail.com>
2022-05-12 13:33:21 +0700

    talitha read drug approvals

--- homepage/new_drugs.php
+++ homepage/new_drugs.php
@@ -1,3 +1,15 @@
+<?php
+/* session_start();
+   if(empty($_SESSION['level'])) {
+      echo "<script>alert('Sorry, you are not allowed to access this page.'); document.location='.pages/login.php'</script>";
+   } */
+$page = "New Drug Approvals";
+require_once '../admin/includes/koneksi.php';
+
+/* $username = $_SESSION['username']; */
+$query = mysqli_query($koneksi, "SELECT * FROM drug_approval where approval_id = 'approval_id'");
+$row = mysqli_fetch_array($query);
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,7 +17,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <title>Drugs Approval</title>
+    <title>Drugs.com - <?=$page;?></title>
+    <link rel="stylesheet" href="css/style1.css">
     <link rel="icon" href="img/drugs1.png" type="image/png">
     <script src="https://kit.fontawesome.com/769f430edb.js" crossorigin="anonymous"></script>
     <style>
@@ -36,22 +49,26 @@ include 'layout/header.php';
                 <div class="ddc-media-list ddc-mgt-4">
                     <div class="ddc-media">
                         <div class="ddc-media-content">
-                            <h2 class="ddc-media-title"><a href="/pro/alymsys.html">Alymsys</a> (bevacizumab-maly)  Injection</h2>
+                        <?php
+                        require_once'../admin/includes/koneksi.php';
+                        $query = "SELECT drug_approval.new_drug, drug_approval.company, drug_approval.date_approval, drug_approval.content, treatment.treatment_name FROM drug_approval JOIN treatment on drug_approval.treatment_id = treatment.treatment_id";
+                        $hasil = mysqli_query($koneksi, $query);
+                        foreach ($hasil as $data){
+                        ?>
+                            <h2 class="ddc-media-title"><a href="/pro/alymsys.html"><?=$data['new_drug']; ?></a></h2>
                                 <p class="drug-subtitle">
-                                <b>Company:</b> <a href="/manufacturer/amneal-pharmaceuticals-llc-19.html">Amneal Pharmaceuticals LLC</a><br>
-                                <b>Date of Approval:</b> April 13, 2022<br>
-                                <b>Treatment for:</b> Colorectal Cancer, Non-Small Cell Lung Cancer, Glioblastoma Multiforme, Renal Cell Carcinoma, Cervical Cancer, Ovarian Cancer, Fallopian Tube Cancer, Peritoneal Cancer<br>
+                                <b>Company:</b><a href="/manufacturer/amneal-pharmaceuticals-llc-19.html"><?=$data['company'];?></a><br>
+                                <b>Date of Approval:</b><?=date('F d, Y', strtotime($data["date_approval"])); ?><br>
+                                <b>Treatment for:</b><?=$data['treatment_name']; ?><br>
                                 </p>
-                                <p>Alymsys (bevacizumab-maly) is a vascular endothelial growth factor inhibitor biosimilar to Avastin (bevacizumab) for the treatment of multiple types of cancer including metastatic colorectal cancer; non-small cell lung cancer; glioblastoma; metastatic renal cell carcinoma; cervical cancer; and epithelial ovarian, fallopian tube, or primary peritoneal cancer.</p>
-                                <ul class="previous_application">
-                                    <li><a href="/newdrugs/fda-approves-alymsys-bevacizumab-maly-biosimilar-avastin-5819.html">FDA Approves Alymsys (bevacizumab-maly), a Biosimilar to Avastin</a> - April 13, 2022</li>
-                                    <li><a href="/history/alymsys.html">Alymsys FDA Approval History</a></li>
-                                </ul>
+                                <p><?=$data['content']; ?></p>
+                        <?php } ?>
                         </div>
+                        
                     </div>
                 </div>
             </div>
-            
+                    
             </div>
 
         <div class="col-md-4 py-5">
@@ -92,6 +109,9 @@ include 'layout/header.php';
         </div>
         </div>
     </div>
+<?php
+require_once 'layout/footer.php';
+?>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
 </body>
