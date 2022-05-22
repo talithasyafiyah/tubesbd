aeca18164082408745c2ca9cbf42c6555cc9f932
talithasyafiyah <talithasyafiyah1112@gmail.com>
2022-05-22 16:29:31 +0700

    talitha

--- homepage/index.php
+++ homepage/index.php
@@ -95,12 +95,11 @@ if(empty($_SESSION['level'])) {
         <div class="row">
             <div class="col">
                 <div class="row">
-                    <div class="btn-group" role="group" aria-label="Basic radio toggle button group">
-                        <input type="radio" class="btn-check" name="btnradio" id="btnradio1" autocomplete="off" checked>
-                        <label class="btn btn-outline-dark btn-lg" for="btnradio1">Browse Drugs</label>
-
-                        <input type="radio" class="btn-check" name="btnradio" id="btnradio2" autocomplete="off">
-                        <label class="btn btn-outline-dark btn-lg" for="btnradio2">Browse Conditions</label>
+                    <div class="col">
+                        <div class="btn-group" role="group" aria-label="Basic radio toggle button group">
+                            <input type="radio" class="btn-check" name="btnradio" id="btnradio1" autocomplete="off" checked>
+                            <label class="btn btn-outline-dark btn-lg" for="btnradio1">Browse Drugs</label>
+                        </div>
                     </div>
                 </div>
                 <div class="row mt-3">
