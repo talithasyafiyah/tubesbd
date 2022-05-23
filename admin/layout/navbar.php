184a1cb1ced0f37fb62d7fd98b546ccf94a7ae43
talithasyafiyah <talithasyafiyah1112@gmail.com>
2022-05-23 14:12:35 +0700

    lagi

--- admin/layout/navbar.php
+++ admin/layout/navbar.php
@@ -148,10 +148,6 @@
                                     </li>
                                     <li><a class="dropdown-item" href="#"><i class="icon-mid bi bi-person me-2"></i> My
                                             Profile</a></li>
-                                    <li><a class="dropdown-item" href="#"><i class="icon-mid bi bi-gear me-2"></i>
-                                            Settings</a></li>
-                                    <li><a class="dropdown-item" href="#"><i class="icon-mid bi bi-wallet me-2"></i>
-                                            Wallet</a></li>
                                     <li>
                                         <hr class="dropdown-divider">
                                     </li>
