c8c8ea22cf5a990626d354013b69088704e160a8
talithasyafiyah <talithasyafiyah1112@gmail.com>
2022-05-23 14:12:05 +0700

    talitha dash admin

--- admin/layout/navbar.php
+++ admin/layout/navbar.php
@@ -144,7 +144,7 @@
                                 </a>
                                 <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton">
                                     <li>
-                                        <h6 class="dropdown-header">Hello, John!</h6>
+                                        <h6 class="dropdown-header">Hello, <?=$_SESSION['username']?>!</h6>
                                     </li>
                                     <li><a class="dropdown-item" href="#"><i class="icon-mid bi bi-person me-2"></i> My
                                             Profile</a></li>
